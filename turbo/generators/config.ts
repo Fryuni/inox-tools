6249e55a6d569c77cde70550e72faae37f318df8
Luiz Ferraz <luiz@lferraz.com>
2024-01-06 13:24:18 -0300

    Add basic configuration for generating new Vite plugins

---
+++ turbo/generators/config.ts
@@ -0,0 +1,14 @@
+import type { PlopTypes } from "@turbo/gen";
+
+Object.assign(console, new console.Console({
+  stdout: process.stdout,
+  stderr: process.stderr,
+  inspectOptions: {
+    colors: true,
+    depth: 4,
+  },
+}));
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.load('./app/index.ts');
+}

555fd1a8f8a3f91a79a5db4a0b5b5cbf84a1473d
Luiz Ferraz <luiz@lferraz.com>
2024-02-02 15:36:54 -0300

    Refactor classes test

--- packages/inline-mod/tests/functions.test.ts
+++ packages/inline-mod/tests/functions.test.ts
@@ -2,11 +2,11 @@ import { expect, test } from 'vitest';
 import { inspectInlineMod } from '../src/inlining.js';
 
 test('arrow function', async () => {
-	const module = await inspectInlineMod({
-		defaultExport: (value: string) => value + 'foo',
-	});
+  const module = await inspectInlineMod({
+    defaultExport: (value: string) => value + 'foo',
+  });
 
-	expect(module.text).toEqualIgnoringWhitespace(`
+  expect(module.text).toEqualIgnoringWhitespace(`
     const __f0 = (value) => value + "foo";
 
     export default __f0;
@@ -14,16 +14,16 @@ test('arrow function', async () => {
 });
 
 test('capturing object', async () => {
-	const suffixes: Partial<Record<string, string>> = {
-		foo: 'bar',
-		baz: 'qux',
-	};
+  const suffixes: Partial<Record<string, string>> = {
+    foo: 'bar',
+    baz: 'qux',
+  };
 
-	const module = await inspectInlineMod({
-		defaultExport: (value: string) => value + (suffixes[value] ?? ''),
-	});
+  const module = await inspectInlineMod({
+    defaultExport: (value: string) => value + (suffixes[value] ?? ''),
+  });
 
-	expect(module.text).toEqualIgnoringWhitespace(`
+  expect(module.text).toEqualIgnoringWhitespace(`
     const __suffixes = {foo: "bar", baz: "qux"};
     function __f0(__0) {
       return (function() {
@@ -37,16 +37,16 @@ test('capturing object', async () => {
 });
 
 test.skip('partially capturing object', async () => {
-	const suffixes = {
-		foo: 'bar',
-		baz: 'qux',
-	};
+  const suffixes = {
+    foo: 'bar',
+    baz: 'qux',
+  };
 
-	const module = await inspectInlineMod({
-		defaultExport: (value: string) => value + suffixes.foo,
-	});
+  const module = await inspectInlineMod({
+    defaultExport: (value: string) => value + suffixes.foo,
+  });
 
-	expect(module.text).toEqualIgnoringWhitespace(`
+  expect(module.text).toEqualIgnoringWhitespace(`
     const __suffixes = {foo: "bar"};
     function __f0(__0) {
       return (function() {
@@ -59,156 +59,3 @@ test.skip('partially capturing object', async () => {
   `);
 });
 
-test('simple classes definition', async () => {
-	class Foo {
-		public constructor(private value: string) {}
-		public bar() {
-			return this.value;
-		}
-		public baz(value: string) {
-			this.value = value;
-		}
-	}
-
-	const { module, text } = await inspectInlineMod({
-		defaultExport: Foo,
-	});
-
-	const { default: Klass } = (await module.get()) as { default: typeof Foo };
-
-	const instance = new Klass('initial state');
-
-	expect(instance.bar()).toBe('initial state');
-
-	instance.baz('other state');
-
-	expect(instance.bar()).toBe('other state');
-
-	expect(text).toEqualIgnoringWhitespace(`
-    function __f0(__0) {
-      return (function() {
-        return function constructor(value) {
-          this.value = value;
-        };
-      }).apply(undefined, undefined).apply(this, arguments);
-    }
-
-    const __f0_prototype = {};
-
-    const __f1 = function bar() {
-      return this.value;
-    };
-
-    const __f2 = function baz(value) {
-      this.value = value;
-    };
-
-    Object.defineProperty(__f0_prototype, "constructor", {
-      configurable: true,
-      enumerable: false,
-      writable: true,
-      value: __f0
-    });
-
-    Object.defineProperty(__f0_prototype, "bar", {
-      configurable: true,
-      enumerable: false,
-      writable: true,
-      value: __f1
-    });
-
-    Object.defineProperty(__f0_prototype, "baz", {
-      configurable: true,
-      enumerable: false,
-      writable: true,
-      value: __f2
-    });
-
-    Object.defineProperty(__f0, "prototype", {
-      configurable: false,
-      enumerable: false,
-      writable: false,
-      value: __f0_prototype
-    });
-
-    export default __f0;
-  `);
-});
-
-test('simple class instance', async () => {
-	class Foo {
-		public constructor(private value: string) {}
-		public bar() {
-			return this.value;
-		}
-		public baz(value: string) {
-			this.value = value;
-		}
-	}
-
-	const { module, text } = await inspectInlineMod({
-		defaultExport: new Foo('initial state'),
-	});
-
-	const { default: instance } = (await module.get()) as { default: Foo };
-
-	expect(instance.bar()).toBe('initial state');
-
-	instance.baz('other state');
-
-	expect(instance.bar()).toBe('other state');
-
-	expect(text).toEqualIgnoringWhitespace(`
-    const __defaultExport_proto = {};
-
-    const __f0 = function bar() {
-      return this.value;
-    };
-
-    const __f1 = function baz(value) {
-      this.value = value;
-    };
-
-    function __f2(__0) {
-      return (function() {
-        return function constructor(value) {
-          this.value = value;
-        };
-      }).apply(undefined, undefined).apply(this, arguments);
-    }
-
-    Object.defineProperty(__f2, "prototype", {
-      configurable: false,
-      enumerable: false,
-      writable: false,
-      value: __defaultExport_proto
-    });
-
-    Object.defineProperty(__defaultExport_proto, "bar", {
-      configurable: true,
-      enumerable: false,
-      writable: true,
-      value: __f0
-    });
-
-    Object.defineProperty(__defaultExport_proto, "baz", {
-      configurable: true,
-      enumerable: false,
-      writable: true,
-      value: __f1
-    });
-
-    Object.defineProperty(__defaultExport_proto, "constructor", {
-      configurable: true,
-      enumerable: false,
-      writable: true,
-      value: __f2
-    });
-
-    const __defaultExport = Object.create(__defaultExport_proto);
-
-    __defaultExport.value = "initial state";
-
-    export default __defaultExport;
-  `);
-});

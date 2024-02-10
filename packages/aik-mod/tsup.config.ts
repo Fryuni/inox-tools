a0bcd683679573dced267a55fca0e227fc62e13d
Luiz Ferraz <luiz@lferraz.com>
2024-02-10 14:16:43 -0300

    Initial version of AIK plugin

---
+++ packages/aik-mod/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  target: 'node18',
+  bundle: true,
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  splitting: false,
+  minify: true,
+  external: ['astro-integration-kit'],
+  tsconfig: 'tsconfig.json',
+});

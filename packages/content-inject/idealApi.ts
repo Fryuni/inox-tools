7f801f218ea5344587bef477b39eb5881e6beb35
Luiz Ferraz <luiz@lferraz.com>
2024-05-01 20:50:19 -0300

    chore: Add example of an ideal API to strive for

---
+++ packages/content-inject/idealApi.ts
@@ -0,0 +1,35 @@
+import { externalCollections, darculaColorCollection } from '@it-astro:content-inject';
+import { defineCollection } from 'astro:content';
+import 'astro/env'; // Don't do this
+
+
+export const idea6 = {
+  docs: defineCollection({
+
+  }),
+  ...externalCollections({
+    darcula: {
+      colors: {
+        extend: schema => schema.and(),
+      },
+    },
+    catpuccin: {},
+  }),
+};
+
+// Without any extra config
+export const idea7 = {
+  docs: defineCollection({
+
+  }),
+}
+
+// With some config to disable the auto-injection
+export const idea7a = {
+  darculaColors: darculaColorCollection({
+
+  }),
+  docs: defineCollection({
+
+  }),
+}

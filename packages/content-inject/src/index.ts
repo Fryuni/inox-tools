6e032456f4cedf17f244a00198d368e26336e73d
Luiz Ferraz <luiz@lferraz.com>
2024-05-01 21:38:35 -0300

    --wip-- [skip ci]

--- packages/content-inject/src/index.ts
+++ packages/content-inject/src/index.ts
@@ -1,12 +1,1 @@
-import { defineIntegration } from 'astro-integration-kit';
-import { z } from 'astro/zod';
-
-
-
-export default defineIntegration({
-  name: '@demo/content-inject',
-  optionsSchema: z.never().optional(),
-  setup() {
-    // TODO: Implement this
-  },
-});
+export * from './utility.js';
--- packages/content-inject/src/utility.ts
+++ packages/content-inject/src/utility.ts
@@ -1,7 +1,15 @@
 import { addVitePlugin, defineUtility } from "astro-integration-kit";
 
+export type Options = {
+  entrypoint: string,
+};
+
 export const injectContent = defineUtility('astro:config:setup')(
-  (params, collectionDefinition) => {
-    addVitePlugin()
+  (params, options: Options) => {
+    addVitePlugin(params, {
+      plugin: {
+        name: '',
+      },
+    });
   }
 );

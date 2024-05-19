204b0ad5586f67a19d38e8946e35d6bcfe9442e5
Luiz Ferraz <luiz@lferraz.com>
2024-05-19 13:07:06 -0300

    fix(aik-route-config): Fixed compatibility with compiler v2.8 (#106)
    Co-authored-by: Adam Matthiesen <30383579+Adammatthiesen@users.noreply.github.com>
    Co-authored-by: Bryce Russell <19967622+BryceRussell@users.noreply.github.com>
    Co-authored-by: Florian Lefebvre <69633530+florian-lefebvre@users.noreply.github.com>

--- packages/aik-route-config/__tests__/hoist.test.ts
+++ packages/aik-route-config/__tests__/hoist.test.ts
@@ -26,51 +26,44 @@ test('hoist simple call', async () => {
 	});
 
 	expect(result?.code).toMatchInlineSnapshot(`
-    "import {
-      Fragment,
-      render as $$render,
-      createAstro as $$createAstro,
-      createComponent as $$createComponent,
-      renderComponent as $$renderComponent,
-      renderHead as $$renderHead,
-      maybeRenderHead as $$maybeRenderHead,
-      unescapeHTML as $$unescapeHTML,
-      renderSlot as $$renderSlot,
-      mergeSlots as $$mergeSlots,
-      addAttribute as $$addAttribute,
-      spreadAttributes as $$spreadAttributes,
-      defineStyleVars as $$defineStyleVars,
-      defineScriptVars as $$defineScriptVars,
-      renderTransition as $$renderTransition,
-      createTransitionScope as $$createTransitionScope,
-      renderScript as $$renderScript,
-      createMetadata as $$createMetadata
-    } from "astro/runtime/server/index.js";
-    import customName from 'magic:hoist';
+		"import {
+		  Fragment,
+		  render as $$render,
+		  createAstro as $$createAstro,
+		  createComponent as $$createComponent,
+		  renderComponent as $$renderComponent,
+		  renderHead as $$renderHead,
+		  maybeRenderHead as $$maybeRenderHead,
+		  unescapeHTML as $$unescapeHTML,
+		  renderSlot as $$renderSlot,
+		  mergeSlots as $$mergeSlots,
+		  addAttribute as $$addAttribute,
+		  spreadAttributes as $$spreadAttributes,
+		  defineStyleVars as $$defineStyleVars,
+		  defineScriptVars as $$defineScriptVars,
+		  renderTransition as $$renderTransition,
+		  createTransitionScope as $$createTransitionScope,
+		  renderScript as $$renderScript,
+		  createMetadata as $$createMetadata
+		} from "astro/runtime/server/index.js";
+		import customName from 'magic:hoist';
 
 
-    import * as $$module1 from 'magic:hoist';
+		import * as $$module1 from 'magic:hoist';
 
-    export const $$metadata = $$createMetadata("<stdin>", { modules: [{ module: $$module1, specifier: 'magic:hoist', assert: {} }], hydratedComponents: [], clientOnlyComponents: [], hydrationDirectives: new Set([]), hoisted: [] });
+		export const $$metadata = $$createMetadata("<stdin>", { modules: [{ module: $$module1, specifier: 'magic:hoist', assert: {} }], hydratedComponents: [], clientOnlyComponents: [], hydrationDirectives: new Set([]), hoisted: [] });
 
-    const $$Astro = $$createAstro();
+		await customName({
+		  bundleFile: import.meta.url,
+		  sourceFile: "/src/pages/simple.astro"
+		}, 'hosted value');
 
-    await customName({
-      bundleFile: import.meta.url,
-      sourceFile: "/src/pages/simple.astro"
-    }, 'hosted value');
-
-    const Astro = $$Astro;
-    const $$stdin = $$createComponent(async ($$result, $$props, $$slots) => {
-      const Astro = $$result.createAstro($$Astro, $$props, $$slots);
-      Astro.self = $$stdin;
-
-
-      return $$render\`\${$$maybeRenderHead($$result)}<p>Simple page</p>\`;
-    }, '<stdin>', undefined);
-    export default $$stdin;
-    "
-  `);
+		const $$stdin = $$createComponent(($$result, $$props, $$slots) => {
+		  return $$render\`\${$$maybeRenderHead($$result)}<p>Simple page</p>\`;
+		}, '<stdin>', undefined);
+		export default $$stdin;
+		"
+	`);
 });
 
 test('hoist awaited call', async () => {
@@ -84,51 +77,44 @@ test('hoist awaited call', async () => {
 	});
 
 	expect(result?.code).toMatchInlineSnapshot(`
-    "import {
-      Fragment,
-      render as $$render,
-      createAstro as $$createAstro,
-      createComponent as $$createComponent,
-      renderComponent as $$renderComponent,
-      renderHead as $$renderHead,
-      maybeRenderHead as $$maybeRenderHead,
-      unescapeHTML as $$unescapeHTML,
-      renderSlot as $$renderSlot,
-      mergeSlots as $$mergeSlots,
-      addAttribute as $$addAttribute,
-      spreadAttributes as $$spreadAttributes,
-      defineStyleVars as $$defineStyleVars,
-      defineScriptVars as $$defineScriptVars,
-      renderTransition as $$renderTransition,
-      createTransitionScope as $$createTransitionScope,
-      renderScript as $$renderScript,
-      createMetadata as $$createMetadata
-    } from "astro/runtime/server/index.js";
-    import customName from 'magic:hoist';
+		"import {
+		  Fragment,
+		  render as $$render,
+		  createAstro as $$createAstro,
+		  createComponent as $$createComponent,
+		  renderComponent as $$renderComponent,
+		  renderHead as $$renderHead,
+		  maybeRenderHead as $$maybeRenderHead,
+		  unescapeHTML as $$unescapeHTML,
+		  renderSlot as $$renderSlot,
+		  mergeSlots as $$mergeSlots,
+		  addAttribute as $$addAttribute,
+		  spreadAttributes as $$spreadAttributes,
+		  defineStyleVars as $$defineStyleVars,
+		  defineScriptVars as $$defineScriptVars,
+		  renderTransition as $$renderTransition,
+		  createTransitionScope as $$createTransitionScope,
+		  renderScript as $$renderScript,
+		  createMetadata as $$createMetadata
+		} from "astro/runtime/server/index.js";
+		import customName from 'magic:hoist';
 
 
-    import * as $$module1 from 'magic:hoist';
+		import * as $$module1 from 'magic:hoist';
 
-    export const $$metadata = $$createMetadata("<stdin>", { modules: [{ module: $$module1, specifier: 'magic:hoist', assert: {} }], hydratedComponents: [], clientOnlyComponents: [], hydrationDirectives: new Set([]), hoisted: [] });
+		export const $$metadata = $$createMetadata("<stdin>", { modules: [{ module: $$module1, specifier: 'magic:hoist', assert: {} }], hydratedComponents: [], clientOnlyComponents: [], hydrationDirectives: new Set([]), hoisted: [] });
 
-    const $$Astro = $$createAstro();
+		await customName({
+		  bundleFile: import.meta.url,
+		  sourceFile: "/src/pages/simple.astro"
+		}, 'hosted value');
 
-    await customName({
-      bundleFile: import.meta.url,
-      sourceFile: "/src/pages/simple.astro"
-    }, 'hosted value');
-
-    const Astro = $$Astro;
-    const $$stdin = $$createComponent(async ($$result, $$props, $$slots) => {
-      const Astro = $$result.createAstro($$Astro, $$props, $$slots);
-      Astro.self = $$stdin;
-
-
-      return $$render\`\${$$maybeRenderHead($$result)}<p>Simple page</p>\`;
-    }, '<stdin>', undefined);
-    export default $$stdin;
-    "
-  `);
+		const $$stdin = $$createComponent(async ($$result, $$props, $$slots) => {
+		  return $$render\`\${$$maybeRenderHead($$result)}<p>Simple page</p>\`;
+		}, '<stdin>', undefined);
+		export default $$stdin;
+		"
+	`);
 });
 
 test('hoist calls mixed with other exports and scopes', async () => {
@@ -142,68 +128,62 @@ test('hoist calls mixed with other exports and scopes', async () => {
 	});
 
 	expect(result?.code).toMatchInlineSnapshot(`
-    "import {
-      Fragment,
-      render as $$render,
-      createAstro as $$createAstro,
-      createComponent as $$createComponent,
-      renderComponent as $$renderComponent,
-      renderHead as $$renderHead,
-      maybeRenderHead as $$maybeRenderHead,
-      unescapeHTML as $$unescapeHTML,
-      renderSlot as $$renderSlot,
-      mergeSlots as $$mergeSlots,
-      addAttribute as $$addAttribute,
-      spreadAttributes as $$spreadAttributes,
-      defineStyleVars as $$defineStyleVars,
-      defineScriptVars as $$defineScriptVars,
-      renderTransition as $$renderTransition,
-      createTransitionScope as $$createTransitionScope,
-      renderScript as $$renderScript,
-      createMetadata as $$createMetadata
-    } from "astro/runtime/server/index.js";
-    import customName from 'magic:hoist';
+		"import {
+		  Fragment,
+		  render as $$render,
+		  createAstro as $$createAstro,
+		  createComponent as $$createComponent,
+		  renderComponent as $$renderComponent,
+		  renderHead as $$renderHead,
+		  maybeRenderHead as $$maybeRenderHead,
+		  unescapeHTML as $$unescapeHTML,
+		  renderSlot as $$renderSlot,
+		  mergeSlots as $$mergeSlots,
+		  addAttribute as $$addAttribute,
+		  spreadAttributes as $$spreadAttributes,
+		  defineStyleVars as $$defineStyleVars,
+		  defineScriptVars as $$defineScriptVars,
+		  renderTransition as $$renderTransition,
+		  createTransitionScope as $$createTransitionScope,
+		  renderScript as $$renderScript,
+		  createMetadata as $$createMetadata
+		} from "astro/runtime/server/index.js";
+		import customName from 'magic:hoist';
 
 
-    import * as $$module1 from 'magic:hoist';
+		import * as $$module1 from 'magic:hoist';
 
-    export const $$metadata = $$createMetadata("<stdin>", { modules: [{ module: $$module1, specifier: 'magic:hoist', assert: {} }], hydratedComponents: [], clientOnlyComponents: [], hydrationDirectives: new Set([]), hoisted: [] });
+		export const $$metadata = $$createMetadata("<stdin>", { modules: [{ module: $$module1, specifier: 'magic:hoist', assert: {} }], hydratedComponents: [], clientOnlyComponents: [], hydrationDirectives: new Set([]), hoisted: [] });
 
-    const $$Astro = $$createAstro();
+		export function getStaticPaths() {
+			customName({
+		        bundleFile: import.meta.url,
+		        sourceFile: "/src/pages/simple.astro"
+		    }, 'should be extended in-place');
+			return [];
+		}
 
-    await customName({
-        bundleFile: import.meta.url,
-        sourceFile: "/src/pages/simple.astro"
-    }, 'hoisted value');
+		await customName({
+		    bundleFile: import.meta.url,
+		    sourceFile: "/src/pages/simple.astro"
+		}, 'hoisted value');
 
-    await customName({
-        bundleFile: import.meta.url,
-        sourceFile: "/src/pages/simple.astro"
-    }, 'second hoisted value');
+		await customName({
+		    bundleFile: import.meta.url,
+		    sourceFile: "/src/pages/simple.astro"
+		}, 'second hoisted value');
 
-    const Astro = $$Astro;
-    export function getStaticPaths() {
-    	customName({
-            bundleFile: import.meta.url,
-            sourceFile: "/src/pages/simple.astro"
-        }, 'should be extended in-place');
-    	return [];
-    }
-    const $$stdin = $$createComponent(async ($$result, $$props, $$slots) => {
-        const Astro = $$result.createAstro($$Astro, $$props, $$slots);
-        Astro.self = $$stdin;
-
-
-        function notExported() {
-            void 0;
-        }
+		const $$stdin = $$createComponent(($$result, $$props, $$slots) => {
+		    function notExported() {
+		        void 0;
+		    }
 
 
-        return $$render\`\${$$maybeRenderHead($$result)}<p>Simple page</p>\`;
-    }, '<stdin>', undefined);
-    export default $$stdin;
-    "
-  `);
+		    return $$render\`\${$$maybeRenderHead($$result)}<p>Simple page</p>\`;
+		}, '<stdin>', undefined);
+		export default $$stdin;
+		"
+	`);
 });
 
 test('hoist calls mixed with adversary exports', async () => {
@@ -256,24 +236,18 @@ test('hoist calls mixed with adversary exports', async () => {
 
 		export const $$metadata = $$createMetadata("<stdin>", { modules: [{ module: $$module1, specifier: 'magic:hoist', assert: {} }], hydratedComponents: [], clientOnlyComponents: [], hydrationDirectives: new Set([]), hoisted: [] });
 
-		const $$Astro = $$createAstro();
+		export function getStaticPaths() {
+			const bar: string[] = [];
+
+			return bar.map((x) => x);
+		}
 
 		await customName({
 		  bundleFile: import.meta.url,
 		  sourceFile: "/src/pages/simple.astro"
 		}, 'second hoisted value');
 
-		const Astro = $$Astro;
-		export function getStaticPaths() {
-			const bar: string[] = [];
-
-			return bar.map((x) => x);
-		}
-		const $$stdin = $$createComponent(async ($$result, $$props, $$slots) => {
-		  const Astro = $$result.createAstro($$Astro, $$props, $$slots);
-		  Astro.self = $$stdin;
-
-
+		const $$stdin = $$createComponent(($$result, $$props, $$slots) => {
 		  const foo: string[] = $$createComponent([]);
 
 

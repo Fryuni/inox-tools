1ba729c5f1606321d6e8e37e03d463146c016367
Luiz Ferraz <luiz@lferraz.com>
2024-07-06 23:40:45 -0300

    chore: Update templates

--- turbo/generators/integration/index.ts
+++ turbo/generators/integration/index.ts
@@ -4,44 +4,44 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 
 export default (plop: PlopTypes.NodePlopAPI, { destBasePath }: PlopTypes.PlopCfg) => {
-  plop.setGenerator('integration', {
-    description: 'Generate a new integration',
-    prompts: [
-      {
-        name: 'name',
-        message: 'Integration name:',
-        validate: async (name) => {
-          const packageJsonPath = path.join(
-            destBasePath,
-            'packages',
-            kebabCase(name),
-            'package.json'
-          );
+	plop.setGenerator('integration', {
+		description: 'Generate a new integration',
+		prompts: [
+			{
+				name: 'name',
+				message: 'Integration name:',
+				validate: async (name) => {
+					const packageJsonPath = path.join(
+						destBasePath,
+						'packages',
+						kebabCase(name),
+						'package.json'
+					);
 
-          try {
-            await fs.lstat(packageJsonPath);
+					try {
+						await fs.lstat(packageJsonPath);
 
-            return `Package ${name} already exists`;
-          } catch {
-            return true;
-          }
-        },
-      },
-      {
-        name: 'description',
-        message: 'Description:',
-        filter: (desc) => desc || undefined,
-        transformer: (desc) => desc || '<empty>',
-      },
-    ],
-    actions: [
-      {
-        type: 'addMany',
-        destination: '{{ turbo.paths.root }}/packages/{{ dashCase name }}',
-        templateFiles: `${__dirname}/templates/**/*`,
-        base: `${__dirname}/templates`,
-        verbose: true,
-      },
-    ],
-  });
+						return `Package ${name} already exists`;
+					} catch {
+						return true;
+					}
+				},
+			},
+			{
+				name: 'description',
+				message: 'Description:',
+				filter: (desc) => desc || undefined,
+				transformer: (desc) => desc || '<empty>',
+			},
+		],
+		actions: [
+			{
+				type: 'addMany',
+				destination: '{{ turbo.paths.root }}/packages/{{ dashCase name }}',
+				templateFiles: `${__dirname}/templates/**/*`,
+				base: `${__dirname}/templates`,
+				verbose: true,
+			},
+		],
+	});
 };

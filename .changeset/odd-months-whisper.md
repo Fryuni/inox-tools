b4d7a164ccee5af2b40de2de28f814f3642858c7
Luiz Ferraz <luiz@lferraz.com>
2024-07-17 21:05:18 -0300

    fix: Fixes `Once` utility and add support for async callbacks

---
+++ .changeset/odd-months-whisper.md
@@ -0,0 +1,5 @@
+---
+'@inox-tools/utils': patch
+---
+
+Fixes `Once` utility and add support for async callbacks
--- packages/utils/src/once.ts
+++ packages/utils/src/once.ts
@@ -1,9 +1,17 @@
 export class Once {
-	readonly #done = false;
+	#done = false;
 
 	public do(callback: () => void): void {
 		if (!this.#done) {
 			callback();
+			this.#done = true;
+		}
+	}
+
+	public async doAsync(callback: () => Promise<void>): Promise<void> {
+		if (!this.#done) {
+			await callback();
+			this.#done = true;
 		}
 	}
 }

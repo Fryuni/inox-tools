98ea8cf8dc1fd34b75028cff49209d1125e19751
Luiz Ferraz <luiz@lferraz.com>
2024-08-16 20:09:22 -0300

    fix(request-state): Fix state revivers

--- packages/request-state/src/runtime/clientState.ts
+++ packages/request-state/src/runtime/clientState.ts
@@ -1,27 +1,17 @@
 import { parse } from 'devalue';
 
-const wellKnownSymbols = new Map(
-	Object.entries(Symbol)
-		.filter(([key, value]) => typeof value === 'symbol' && typeof key === 'string')
-		.map(([key, value]) => [value, key])
-);
-
-const reducers: Record<string, (value: any) => any> = {
-	URL: (value) => value instanceof URL && value.href,
-	Date: (value) => value instanceof Date && value.valueOf(),
-	GlobalSymbol: (value) =>
-		typeof value === 'symbol' &&
-		value.description !== undefined &&
-		value === Symbol.for(value.description) &&
-		value.description,
-	WellKnownSymbol: (value) => typeof value === 'symbol' && wellKnownSymbols.get(value),
+const revivers: Record<string, (value: any) => any> = {
+	URL: (value) => new URL(value),
+	Date: (value) => new Date(value),
+	GlobalSymbol: (value) => Symbol.for(value),
+	WellKnownSymbol: (value) => Symbol[value as keyof typeof Symbol],
 };
 
 const loadState = (): Map<string, unknown> => {
 	const element = document.getElementById('it-astro-state');
 
 	if (element?.textContent) {
-		const state = parse(element.textContent);
+		const state = parse(element.textContent, revivers);
 		element.remove();
 		return state;
 	}

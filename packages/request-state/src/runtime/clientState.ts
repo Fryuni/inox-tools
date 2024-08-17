bbe0756d897fda2116431be49eeb3512d07f8c2b
Luiz Ferraz <luiz@lferraz.com>
2024-08-17 11:45:56 -0300

    feat(request-state): Add support for ViewTransitions

--- packages/request-state/src/runtime/clientState.ts
+++ packages/request-state/src/runtime/clientState.ts
@@ -7,19 +7,34 @@ const revivers: Record<string, (value: any) => any> = {
 	WellKnownSymbol: (value) => Symbol[value as keyof typeof Symbol],
 };
 
-const loadState = (): Map<string, unknown> => {
-	const element = document.getElementById('it-astro-state');
+type State = Map<string, unknown>;
+
+let nextState: State | undefined = undefined;
+
+document.addEventListener('astro:before-swap', (event) => {
+	nextState = loadState(event.newDocument);
+});
+
+document.addEventListener('astro:after-swap', () => {
+	state.clear();
+	if (nextState !== undefined) {
+		for (const [key, value] of nextState) {
+			state.set(key, value);
+		}
+	}
+});
+
+const loadState = (doc: Document): State | undefined => {
+	const element = doc.getElementById('it-astro-state');
 
 	if (element?.textContent) {
 		const state = parse(element.textContent, revivers);
 		element.remove();
 		return state;
 	}
-
-	return new Map();
 };
 
-const state = loadState();
+const state = loadState(document) ?? new Map();
 
 export const getState = (key: string, valueIfMissing?: unknown): unknown => {
 	if (!state.has(key)) {

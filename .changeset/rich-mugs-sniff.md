fa581d2087846799b1bd001f7a82ecfc33999803
Luiz Ferraz <luiz@lferraz.com>
2024-08-22 21:43:32 -0300

    feat(content-utils): Optimize Git time retrieval

---
+++ .changeset/rich-mugs-sniff.md
@@ -0,0 +1,5 @@
+---
+'@inox-tools/content-utils': patch
+---
+
+Optimize git times to use a single git command instead of one per file
--- packages/content-utils/src/runtime/git.ts
+++ packages/content-utils/src/runtime/git.ts
@@ -1,5 +1,5 @@
 import { spawnSync } from 'node:child_process';
-import { basename, dirname, join, sep, resolve } from 'node:path';
+import { basename, dirname, join, sep, resolve, relative } from 'node:path';
 import { hooks } from '@inox-tools/modular-station/hooks';
 
 let contentPath: string = '';
@@ -21,7 +21,7 @@ export async function getCommitDate(file: string, age: 'oldest' | 'latest'): Pro
 		args.push('--follow', '--diff-filter=A');
 	}
 
-	args.push(basename(file));
+	args.push('--', basename(file));
 
 	const result = spawnSync('git', args, {
 		cwd: resolve(join(contentPath, dirname(file))),
@@ -92,6 +92,19 @@ export async function listGitTrackedFiles(): Promise<string[]> {
 	return files;
 }
 
+function getRepoRoot(): string {
+	const result = spawnSync('git', ['rev-parse', '--show-toplevel'], {
+		cwd: contentPath,
+		encoding: 'utf-8',
+	});
+
+	if (result.error) {
+		return contentPath;
+	}
+
+	return result.stdout.trim();
+}
+
 type TrackedCommits = {
 	oldest: [string, Date][];
 	latest: [string, Date][];
@@ -101,7 +114,58 @@ type TrackedCommits = {
  * @internal
  */
 export async function getAllTrackedCommitDates(): Promise<TrackedCommits> {
+	const repoRoot = getRepoRoot();
 	const trackedFiles = await listGitTrackedFiles();
+
+	const args = ['log', '--format=t:%ct', '--name-status', '--', contentPath];
+
+	const gitLog = spawnSync('git', args, {
+		cwd: repoRoot,
+		encoding: 'utf-8',
+	});
+
+	if (gitLog.error) {
+		return {
+			oldest: [],
+			latest: [],
+		};
+	}
+
+	const logLines = gitLog.stdout.trim().split('\n');
+
+	let runningDate: number = Date.now();
+	const runningMinMax = new Map<string, { min: number; max: number }>();
+
+	for (const logLine of logLines) {
+		if (logLine.startsWith('t:')) {
+			// t:<seconds since epoch>
+			runningDate = Number.parseInt(logLine.slice(2)) * 1000;
+		}
+
+		// - Added files take the format `A\t<file>`
+		// - Modified files take the format `M\t<file>`
+		// - Deleted files take the format `D\t<file>`
+		// - Renamed files take the format `R<count>\t<old>\t<new>`
+		// - Copied files take the format `C<count>\t<old>\t<new>`
+		// The name of the file as of the commit being processed is always
+		// the last part of the log line.
+		const tabSplit = logLine.lastIndexOf('\t');
+		if (tabSplit === -1) continue;
+		const fileName = logLine.slice(tabSplit + 1);
+
+		const minMax = runningMinMax.get(fileName);
+
+		if (minMax === undefined) {
+			runningMinMax.set(fileName, {
+				min: runningDate,
+				max: runningDate,
+			});
+			continue;
+		}
+
+		minMax.min = Math.min(minMax.min, runningDate);
+		minMax.max = Math.max(minMax.max, runningDate);
+	}
 
 	const result: TrackedCommits = {
 		oldest: [],
@@ -109,11 +173,18 @@ export async function getAllTrackedCommitDates(): Promise<TrackedCommits> {
 	};
 
 	for (const file of trackedFiles) {
+		// git log returns file names relative to the repo root
+		// convert the tracked paths to match that format
+		const repoFilePath = relative(repoRoot, resolve(contentPath, file));
+
+		const minMax = runningMinMax.get(repoFilePath);
+		if (minMax === undefined) continue;
+
 		// Replace the first occurrence of the separator so it doesn't get partially normalized when mixin Windows and Unix-like systems.
 		const name = file.replace(sep, ':');
 
-		result.oldest.push([name, await getCommitDate(file, 'oldest')]);
-		result.latest.push([name, await getCommitDate(file, 'latest')]);
+		result.oldest.push([name, new Date(minMax.min)]);
+		result.latest.push([name, new Date(minMax.max)]);
 	}
 
 	return result;

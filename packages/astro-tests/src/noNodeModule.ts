73adfe7d3f5564eca716e055fbaa74ee42530705
Luiz Ferraz <luiz@lferraz.com>
2024-09-06 10:41:03 -0300

    Link to original repo instead of fork

--- packages/astro-tests/src/noNodeModule.ts
+++ packages/astro-tests/src/noNodeModule.ts
@@ -1,5 +1,5 @@
 /**
- * Copied from: https://github.com/Fryuni/astro/blob/d2574ad8932039f3eea3bd6d9368bec377a0c334/packages/astro/test/test-plugins.js
+ * Copied from: https://github.com/withastro/astro/blob/d2574ad8932039f3eea3bd6d9368bec377a0c334/packages/astro/test/test-plugins.js
  * Modified to check all declared Node built-in modules instead of a small subset.
  */
 
@@ -7,25 +7,25 @@ import type { Plugin } from 'vite';
 import { builtinModules } from 'node:module';
 
 const nodeModules = [
-  ...builtinModules,
-  ...builtinModules.map((modName) => `node:${modName}`),
-  'node:test',
-  'node:test/reporters',
-  'node:sea',
+	...builtinModules,
+	...builtinModules.map((modName) => `node:${modName}`),
+	'node:test',
+	'node:test/reporters',
+	'node:sea',
 ];
 
 export function preventNodeBuiltinDependencyPlugin(): Plugin {
-  // Verifies that the final bundle does not have a hard dependency on Node builtins.
-  // This is to verify it will run on Cloudflare and Deno
-  return {
-    name: 'verify-no-node-stuff',
-    generateBundle() {
-      nodeModules.forEach((name) => {
-        const mod = this.getModuleInfo(name);
-        if (mod) {
-          throw new Error(`Node builtins snuck in: ${name}`);
-        }
-      });
-    },
-  };
+	// Verifies that the final bundle does not have a hard dependency on Node builtins.
+	// This is to verify it will run on Cloudflare and Deno
+	return {
+		name: 'verify-no-node-stuff',
+		generateBundle() {
+			nodeModules.forEach((name) => {
+				const mod = this.getModuleInfo(name);
+				if (mod) {
+					throw new Error(`Node builtins snuck in: ${name}`);
+				}
+			});
+		},
+	};
 }
--- packages/astro-tests/src/testAdapter.ts
+++ packages/astro-tests/src/testAdapter.ts
@@ -1,5 +1,5 @@
 /**
- * Copied from: https://github.com/Fryuni/astro/blob/d2574ad8932039f3eea3bd6d9368bec377a0c334/packages/astro/test/test-adapter.js
+ * Copied from: https://github.com/withastro/astro/blob/d2574ad8932039f3eea3bd6d9368bec377a0c334/packages/astro/test/test-adapter.js
  * Modified to use TypeScript and to comply with new types and requirements.
  */
 
@@ -11,57 +11,58 @@ type MiddlewareEntryPoint = HookParameters<'astro:build:ssr'>['middlewareEntryPo
 type Routes = HookParameters<'astro:build:done'>['routes'];
 
 export type Options = {
-  env?: Record<string, string | undefined>;
-  provideAddress?: boolean;
-  extendAdapter?: AstroAdapter;
-  setEntryPoints?: (entryPoints: EntryPoints) => void;
-  setMiddlewareEntryPoint?: (middlewareEntryPoint: MiddlewareEntryPoint) => void;
-  setRoutes?: (routes: Routes) => void;
+	env?: Record<string, string | undefined>;
+	provideAddress?: boolean;
+	extendAdapter?: AstroAdapter;
+	setEntryPoints?: (entryPoints: EntryPoints) => void;
+	setMiddlewareEntryPoint?: (middlewareEntryPoint: MiddlewareEntryPoint) => void;
+	setRoutes?: (routes: Routes) => void;
 };
 
 export default function (options: Options = {}): AstroIntegration {
-  const {
-    provideAddress = true,
-    extendAdapter,
-    setEntryPoints,
-    setMiddlewareEntryPoint,
-    setRoutes,
-    env = {},
-  } = options;
+	const {
+		provideAddress = true,
+		extendAdapter,
+		setEntryPoints,
+		setMiddlewareEntryPoint,
+		setRoutes,
+		env = {},
+	} = options;
 
-  return {
-    name: 'test-ssr-adapter',
-    hooks: {
-      'astro:config:setup': ({ updateConfig }) => {
-        updateConfig({
-          vite: {
-            plugins: [
-              {
-                name: 'test-ssr-adapter',
-                resolveId(id) {
-                  if (id === '@my-ssr') {
-                    return id;
-                  } else if (id === 'astro/app') {
-                    const viteId = viteID(new URL('../dist/core/app/index.js', import.meta.url));
-                    return viteId;
-                  }
-                },
-                load(id) {
-                  if (id === '@my-ssr') {
-                    return `
+	return {
+		name: 'test-ssr-adapter',
+		hooks: {
+			'astro:config:setup': ({ updateConfig }) => {
+				updateConfig({
+					vite: {
+						plugins: [
+							{
+								name: 'test-ssr-adapter',
+								resolveId(id) {
+									if (id === '@my-ssr') {
+										return id;
+									} else if (id === 'astro/app') {
+										const viteId = viteID(new URL('../dist/core/app/index.js', import.meta.url));
+										return viteId;
+									}
+								},
+								load(id) {
+									if (id === '@my-ssr') {
+										return `
 											import { App } from 'astro/app';
 											import fs from 'fs';
 
-											${env
-                        ? `
+											${
+												env
+													? `
 											await import('astro/env/setup')
 												.then(mod => mod.setGetEnv((key) => {
 													const data = ${JSON.stringify(env)};
 													return data[key];
 												}))
 												.catch(() => {});`
-                        : ''
-                      }
+													: ''
+											}
 
 											class MyApp extends App {
 												#manifest = null;
@@ -90,41 +91,41 @@ export default function (options: Options = {}): AstroIntegration {
 												};
 											}
 										`;
-                  }
-                },
-              },
-            ],
-          },
-        });
-      },
-      'astro:config:done': ({ setAdapter }) => {
-        setAdapter({
-          name: 'my-ssr-adapter',
-          serverEntrypoint: '@my-ssr',
-          exports: ['manifest', 'createApp'],
-          supportedAstroFeatures: {
-            serverOutput: 'stable',
-            envGetSecret: 'experimental',
-            staticOutput: 'stable',
-            hybridOutput: 'stable',
-            i18nDomains: 'stable',
-          },
-          ...extendAdapter,
-        });
-      },
-      'astro:build:ssr': ({ entryPoints, middlewareEntryPoint }) => {
-        if (setEntryPoints) {
-          setEntryPoints(entryPoints);
-        }
-        if (setMiddlewareEntryPoint) {
-          setMiddlewareEntryPoint(middlewareEntryPoint);
-        }
-      },
-      'astro:build:done': ({ routes }) => {
-        if (setRoutes) {
-          setRoutes(routes);
-        }
-      },
-    },
-  };
+									}
+								},
+							},
+						],
+					},
+				});
+			},
+			'astro:config:done': ({ setAdapter }) => {
+				setAdapter({
+					name: 'my-ssr-adapter',
+					serverEntrypoint: '@my-ssr',
+					exports: ['manifest', 'createApp'],
+					supportedAstroFeatures: {
+						serverOutput: 'stable',
+						envGetSecret: 'experimental',
+						staticOutput: 'stable',
+						hybridOutput: 'stable',
+						i18nDomains: 'stable',
+					},
+					...extendAdapter,
+				});
+			},
+			'astro:build:ssr': ({ entryPoints, middlewareEntryPoint }) => {
+				if (setEntryPoints) {
+					setEntryPoints(entryPoints);
+				}
+				if (setMiddlewareEntryPoint) {
+					setMiddlewareEntryPoint(middlewareEntryPoint);
+				}
+			},
+			'astro:build:done': ({ routes }) => {
+				if (setRoutes) {
+					setRoutes(routes);
+				}
+			},
+		},
+	};
 }

34ee22fd0054e6453d5e4c253db12d7bc14798a4
Luiz Ferraz <luiz@lferraz.com>
2024-09-06 11:04:37 -0300

    feat(utils): Add utility to convert Web Stream into Async Iterator

---
+++ packages/utils/src/streams.ts
@@ -0,0 +1,13 @@
+export async function* streamAsyncIterator(stream: ReadableStream) {
+  const reader = stream.getReader();
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) return;
+      yield value;
+    }
+  } finally {
+    reader.releaseLock();
+  }
+}

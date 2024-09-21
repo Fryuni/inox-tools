3d9d36674baf3f7a3ec7f4972f89515e1965b358
Luiz Ferraz <luiz@lferraz.com>
2024-09-21 11:42:19 -0300

    fix: Remove prototype

--- examples/sitemap-ext/src/env.d.ts
+++ examples/sitemap-ext/src/env.d.ts
@@ -1,6 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-declare module '@it-astro:cut-short' {
-  export const endRequest: (withResponse: Response | (() => Promise<Respone> | Response)) => never;
-}

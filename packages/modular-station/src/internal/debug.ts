6bfa74f0712d57dcd77402e8759207ccea31b173
Luiz Ferraz <luiz@lferraz.com>
2024-11-11 10:26:32 -0300

    fix: Remove global debug change

--- packages/modular-station/src/internal/debug.ts
+++ packages/modular-station/src/internal/debug.ts
@@ -1,11 +1,5 @@
 import debugC from 'debug';
 
-debugC.inspectOpts = {
-  colors: true,
-  depth: 10,
-  showHidden: true,
-};
-
 export const debug = debugC('inox-tools:modular-station');
 
 export function getDebug(name: string): debugC.Debugger {

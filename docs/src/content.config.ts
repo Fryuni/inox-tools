9b4c68d83b64dbf4edfe189937f74a1d0f2df052
Luiz Ferraz <luiz@lferraz.com>
2025-01-31 23:40:03 -0300

    docs: Reactivate link validation

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,10 +1,13 @@
-import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
 	docs: defineCollection({
-		loader: docsLoader(),
+		loader: glob({
+			base: 'src/content/docs',
+			pattern: '**',
+		}),
 		schema: docsSchema({
 			extend: z.object({
 				packageName: z.string().optional(),

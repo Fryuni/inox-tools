02f1df8095fd206aff785ffe556ca2d83cf24b46
Luiz Ferraz <luiz@lferraz.com>
2025-02-09 05:23:36 -0300

    feat(content-utils): Add static-only collections (#199)
    Co-authored-by: Oliver Speir <115520730+OliverSpeir@users.noreply.github.com>

---
+++ .changeset/wet-pets-flash.md
@@ -0,0 +1,7 @@
+---
+'@inox-tools/content-utils': minor
+---
+
+Add static only collection feature.
+
+Now users can pass `staticOnlyCollections` to the integration to make the entries of those integration not be shipped in the server bundle.
--- examples/content-injection/astro.config.ts
+++ examples/content-injection/astro.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
 import integration from './integration';
 import runtimeLogger from '@inox-tools/runtime-logger';
+import contentUtils from '@inox-tools/content-utils';
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [runtimeLogger(), integration()],
+	output: 'server',
+	adapter: node({ mode: 'standalone' }),
+	integrations: [
+		runtimeLogger(),
+		contentUtils({
+			staticOnlyCollections: ['blog'],
+		}),
+		integration(),
+	],
 });

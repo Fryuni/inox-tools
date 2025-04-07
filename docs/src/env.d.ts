2ba9b86f905ebe5be9300a6aebef26fbf27a02d4
Luiz Ferraz <luiz@lferraz.com>
2025-04-07 05:02:43 -0300

    docs: Update Starlight overrides

--- docs/src/env.d.ts
+++ docs/src/env.d.ts
@@ -1,9 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@astrojs/starlight/virtual" />
-
-declare namespace App {
-  interface Locals {
-    starlight: import('@astrojs/starlight/props').Props;
-  }
-}

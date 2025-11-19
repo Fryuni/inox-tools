d7fca17048a5436eb93661870de72d46e33374d9
Ivo Evans <ivo.evansstorrie@rvu.co.uk>
2025-11-19 14:39:58 +0000

    Add test that partials are resolved correctly

---
+++ packages/request-nanostores/e2e/fixture/partials/foo-second/astro.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+import requestNanostores from '@inox-tools/request-nanostores';
+
+import preact from '@astrojs/preact';
+
+export default defineConfig({
+	output: 'server',
+	adapter: node({ mode: 'standalone' }),
+	integrations: [requestNanostores(), preact()],
+	devToolbar: { enabled: false },
+	server: {
+		port: 4001
+	},
+	build:{
+		assetsPrefix: "http://localhost:4001/"
+	}
+});
---
+++ packages/request-nanostores/e2e/partials.test.ts
@@ -0,0 +1,53 @@
+import {
+	loadFixture,
+	type DevServer,
+	type PreviewServer,
+} from '@inox-tools/astro-tests/astroFixture';
+import { test, expect } from '@playwright/test';
+
+interface partialServerConfig {
+	name: string;
+	port: number;
+}
+
+const partialServerConfigs: partialServerConfig[] = [
+	{ name: 'foo-first', port: 4000 },
+	{ name: 'foo-second', port: 4001 },
+	{ name: 'bar', port: 4002 },
+];
+
+const partialsProxyFixture = await loadFixture({ root: './fixture/partials/proxy' });
+
+let partialServers: DevServer[];
+let proxyServer: PreviewServer;
+
+test.beforeAll(async () => {
+	partialServers = await Promise.all(
+		partialServerConfigs.map(async ({ name, port }) => {
+			const fixture = await loadFixture({ root: `./fixture/partials/${name}` });
+			return fixture.startDevServer({ server: { port } });
+		})
+	);
+	await partialsProxyFixture.build({});
+	proxyServer = await partialsProxyFixture.preview({});
+});
+
+test.afterAll(async () => {
+	await proxyServer?.stop();
+	await Promise.all(partialServers.map(server => server.stop()))
+});
+
+test('can collect partials from other servers', async ({ page }) => {
+	await page.goto(partialsProxyFixture.resolveUrl('/'));
+
+	const fooFirstState = JSON.parse(await page.locator('pre#injected-state-foo-first').innerHTML());
+	const fooSecondState = JSON.parse(
+		await page.locator('pre#injected-state-foo-second').innerHTML()
+	);
+	const barState = JSON.parse(await page.locator('pre#injected-state-bar').innerHTML());
+
+	expect(fooFirstState).toBe('foo-first');
+	expect(fooSecondState).toBe('foo-first'); // not a mistake - when a partial tries to overwrite an existing partial, it should fail
+	expect(barState).toBe('bar');
+});
+

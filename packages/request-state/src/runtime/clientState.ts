592200aa91acbab71e86805e958232a385b3496b
Ivo Evans <ivo.evansstorrie@rvu.co.uk>
2025-11-20 15:19:28 +0000

    Clean up a bit

--- packages/request-state/src/runtime/clientState.ts
+++ packages/request-state/src/runtime/clientState.ts
@@ -8,7 +8,7 @@ const revivers: Record<string, (value: any) => any> = {
 	WellKnownSymbol: (value) => Symbol[value as keyof typeof Symbol],
 };
 
-const loadState = (doc: Document, caller?: string): State[] => {
+const loadState = (doc: Document): State[] => {
 	const elements = Array.from(doc.querySelectorAll('.it-astro-state'));
 	const scripts = elements
 		.map((element) => {
@@ -18,11 +18,11 @@ const loadState = (doc: Document, caller?: string): State[] => {
 				return state;
 			}
 		})
-		.filter(Boolean);
+		.filter((state): state is State => !!state);
 	return scripts;
 };
 
-let nextStates = loadState(document, 'global');
+let nextStates = loadState(document);
 
 const initialiseState = (): State => {
 	const STATE_WINDOW_NAMESPACE = '__@it-astro:request-state-data' as const;
@@ -53,7 +53,7 @@ const mergeState = (oldState: State, newState: State) => {
 const applyState =
 	(isViewTransition: boolean = false) =>
 	() => {
-		const startingState = isViewTransition ? new Map() : new Map(state); // if it's a view transition, we want a completely new state (otherwise the new view will 'lose' to the old view in the case of a conflict). If it's not a view transition, we want to shallow clone the state object. mergeState will update the top-level keys of the state passed to it, but if the user calls preventDefault on the serverStateLoaded we're supposed to abort the update. So it won't do to have already updated the underlying global state map. And on the other hand we do a shallow, not deep, clone because the user can mutate their own stored values any time they want.
+		const startingState = isViewTransition ? new Map() : new Map(state); // if it's an uncancelled view transition, we want a completely new state (otherwise the new view will 'lose' to the old view in the case of a conflict). If it's not a view transition, we want to shallow clone the state object. mergeState will update the top-level keys of the state passed to it, but if the user calls preventDefault on the serverStateLoaded we're supposed to abort the update. So it won't do to have already updated the underlying global state map. And on the other hand we do a shallow, not deep, clone because the user can mutate their own stored values any time they want.
 		nextStates?.reduce(mergeState, startingState);
 		const event = new ServerStateLoaded(new Map(state), startingState);
 

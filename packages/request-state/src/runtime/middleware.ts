19ab17c71bf1e0165683cd79fe758df14024d9ac
Mads Erik Forberg <forberg@gmail.com>
2026-02-20 10:27:50 +0100

    feat: add content-length header when opting out of stream

--- packages/request-state/src/runtime/middleware.ts
+++ packages/request-state/src/runtime/middleware.ts
@@ -15,12 +15,19 @@ export const onRequest = defineMiddleware(async (_, next) => {
 
 	const originalBody = await result.text();
 
+	let contentLength = Buffer.byteLength(originalBody, 'utf-8');
+	result.headers.set('Content-Length', contentLength.toString());
+
 	const state = getState();
 	const stateScript = state
 		? `<script class="it-astro-state" type="application/json+devalue">${state}</script>`
 		: null;
 	if (stateScript) {
 		const headCloseIndex = originalBody.indexOf('</head>');
+
+		contentLength += Buffer.byteLength(stateScript, 'utf-8');
+		result.headers.set('Content-Length', contentLength.toString());
+
 		if (headCloseIndex > -1) {
 			return new Response(
 				originalBody.slice(0, headCloseIndex) + stateScript + originalBody.slice(headCloseIndex),
